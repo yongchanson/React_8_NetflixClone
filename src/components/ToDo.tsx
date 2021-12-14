3574645ff2a2bc2263b80eb0573e50816d149b8f
yongchanson <syc9430@gmail.com>
2021-12-14 18:15:49 +0900

    memo

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -2,18 +2,28 @@ import React from "react";
 import { useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text, category }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
   const setToDos = useSetRecoilState(toDoState); //atom을 수정가능하게 만든것
   // const onClick = (newCategory: IToDo["category"]) => {
   //   console.log("i wanna to", newCategory);
-  //   console.log("i wanna to", event.currentTarget.name);
+    // console.log("i wanna to", event.currentTarget.name);
   // };
   const onClick = (event:React.MouseEvent<HTMLButtonElement>) => {
   //  event.currentTarget.name
     const {
       currentTarget: { name },
     } = event;
-  }
+    // console.log("i wanna to", event.currentTarget.name);
+    setToDos((oldToDos) => { //toDo의 경로찾기(index)
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const oldToDo = oldToDos[targetIndex];
+      const newToDo = { text, id, category: name }; //category: name 버튼이 준 카테고리받기
+      // console.log(targetIndex);//toDo의 경로찾기(index)
+      console.log(oldToDo, newToDo);
+      return oldToDos;
+    });
+  };
+  
   return (
     <li>
       {/* &&뒤는 조건이 충족되면 실행되는 값이다 */}
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -6,7 +6,7 @@ import ToDo from "./ToDo";
 
 function ToDoList() {
     const toDos = useRecoilValue(toDoState);
-
+    console.log(toDos);
 return(
     <div>
         <h1> ToDo List</h1>
@@ -15,6 +15,7 @@ return(
         
         <ul>
             {toDos.map((toDo) => (<ToDo key={toDo.id} {...toDo} />))}
+            {/* toDo는 모든 props를 받는 상태(3개 : To_Do, DOING, DONE) */}
             {/* {toDos.map((toDo) => (<ToDo text={toDo.text} category={toDo.category} id={toDo.id} />))} */}
             {/* ToDo와 toDoState은 둘다 IToDo타입이라 prop가 같음 / 17~18식은 같은식임 */}
         </ul>
